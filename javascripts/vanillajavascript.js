aa6893b8a1f27aa2dd5d7cf2f865bcc16eb285ac
Ismael Pacheco Torrecilla <ismaelpacheco13001@gmail.com>
2019-10-30 20:59:23 +0100

    V.01

--- javascripts/vanillajavascript.js
+++ javascripts/vanillajavascript.js
@@ -1,6 +1,12 @@
 function show(element) {
-    document.getElementById(element).style.visibility = "visible";
-  }
-  function hide(element) {
-    document.getElementById(element).style.visibility = "hidden";
-  }
+  document.getElementById(element).style.visibility = "visible";
+}
+function hide(element) {
+  document.getElementById(element).style.visibility = "hidden";
+}
+function sonar(element){
+  document.getElementById(element).play();
+}
+function parar(element){
+  document.getElementById(element).pause();
+}
